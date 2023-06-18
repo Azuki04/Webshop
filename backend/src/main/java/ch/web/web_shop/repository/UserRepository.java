16569c942302fd495047be5dcdcf549105bb3077
Azuki04 <syviet@outlook.com>
2023-06-18 10:20:16 +0200

    added User and Role Repository #4

---
+++ backend/src/main/java/ch/web/web_shop/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package ch.web.web_shop.repository;
+
+import ch.web.web_shop.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
