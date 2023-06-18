63b7d5ed44993bdca88941a2cc6232424d026910
Azuki04 <syviet@outlook.com>
2023-06-18 12:53:02 +0200

    debug dto and controller

--- backend/src/main/java/ch/web/web_shop/controller/NewsletterController.java
+++ backend/src/main/java/ch/web/web_shop/controller/NewsletterController.java
@@ -6,7 +6,7 @@ import ch.web.web_shop.service.NewsletterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
