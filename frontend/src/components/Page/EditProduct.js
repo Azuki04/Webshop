dc66a13c53048dad889eb2293a8a7d80cdc4d9d3
Azuki04 <syviet@outlook.com>
2023-06-28 11:44:34 +0200

    added axios for user and Admin  #10

--- frontend/src/components/Page/EditProduct.js
+++ frontend/src/components/Page/EditProduct.js
@@ -2,12 +2,13 @@ import React from "react";
 import axios from "axios";
 import { withRouter } from "../../common/with-router";
 import authHeader from "../services/Auth-header";
-
 import { Button } from "../Button";
+import Auth from "../services/Auth";
 
 class EditProduct extends React.Component {
   constructor(props) {
     super(props);
+    // Bindung der Methoden an die Komponente
     this.changeTitleHandler = this.changeTitleHandler.bind(this);
     this.changeDescriptionHandler = this.changeDescriptionHandler.bind(this);
     this.changeContentHandler = this.changeContentHandler.bind(this);
@@ -31,7 +32,6 @@ class EditProduct extends React.Component {
         stock: "",
         src: "",
         category: "",
-
         published: false,
       },
       categories: [],
@@ -40,12 +40,19 @@ class EditProduct extends React.Component {
   }
 
   componentDidMount() {
-    this.getProduct(this.props.router.params.id);
-    fetch(process.env.REACT_APP_API_URL +"/category")
-      .then((response) => response.json())
-      .then((data) => this.setState({ categories: data }));
+    const currentUser = Auth.getCurrentUser();
+    if (currentUser.roles.includes("ROLE_ADMIN")) {
+      this.getProduct(this.props.router.params.id);
+    } else {
+      this.getUserProduct(this.props.router.params.id);
+    }
+
+    fetch(process.env.REACT_APP_API_URL + "/category")
+        .then((response) => response.json())
+        .then((data) => this.setState({ categories: data }));
   }
 
+  // Handler-Methode für die Änderung des Titels
   changeTitleHandler(event) {
     this.setState((prevState) => ({
       currentProduct: {
@@ -55,6 +62,7 @@ class EditProduct extends React.Component {
     }));
   }
 
+  // Handler-Methode für die Änderung der Beschreibung
   changeDescriptionHandler(event) {
     this.setState((prevState) => ({
       currentProduct: {
@@ -64,273 +72,365 @@ class EditProduct extends React.Component {
     }));
   }
 
-  changeContentHandler = (event) => {
+  // Handler-Methode für die Änderung des Inhalts
+  changeContentHandler(event) {
     this.setState((prevState) => ({
       currentProduct: {
         ...prevState.currentProduct,
         content: event.target.value,
       },
     }));
-  };
+  }
 
-  changePriceHandler = (event) => {
+  // Handler-Methode für die Änderung des Preises
+  changePriceHandler(event) {
     this.setState((prevState) => ({
       currentProduct: {
         ...prevState.currentProduct,
         price: event.target.value,
       },
     }));
-  };
+  }
 
-  changeStockHandler = (event) => {
+  // Handler-Methode für die Änderung des Lagerbestands
+  changeStockHandler(event) {
     this.setState((prevState) => ({
       currentProduct: {
         ...prevState.currentProduct,
         stock: event.target.value,
       },
     }));
-  };
+  }
 
-  changeSrcHandler = (event) => {
+  // Handler-Methode für die Änderung des Bildquellenpfads
+  changeSrcHandler(event) {
     this.setState((prevState) => ({
       currentProduct: {
         ...prevState.currentProduct,
         src: event.target.value,
       },
     }));
-  };
+  }
 
-  changeCategoryHandler = (event) => {
+  // Handler-Methode für die Änderung der Kategorie
+  changeCategoryHandler(event) {
     this.setState({ category: event.target.value });
-  };
+  }
 
-  //get products with axios fetch with jwt token
+  // Produkt abrufen mit Axios-Fetch und JWT-Token
   getProduct(id) {
     const config = {
-      headers:
-          authHeader()
+      headers: authHeader(),
     };
     axios
-        .get(process.env.REACT_APP_API_URL +"/products/admin/" + id, config)
+        .get(process.env.REACT_APP_API_URL + "/products/admin/" + id, config)
         .then((response) => {
-          this.setState({
-            currentProduct: response.data,
-            });
-            console.log(response.data); // response.data is the product
+          this.setState({ currentProduct: response.data });
+        })
+        .catch((err) => {
+          console.log(err);
         });
   }
 
-  // publish fetch product with jwt token
-  updatePublished(status) {
+  // Produkt des Benutzers abrufen mit Axios-Fetch und JWT-Token
+  getUserProduct(id) {
+    const currentUser = Auth.getCurrentUser();
+
     const config = {
-      headers:
-          authHeader()
+      headers: authHeader(),
     };
-    var data = {
-      id: this.state.currentProduct.id,
-      title: this.state.currentProduct.title,
-      description: this.state.currentProduct.description,
-      content: this.state.currentProduct.content,
-      price: this.state.currentProduct.price,
-      stock: this.state.currentProduct.stock,
-      src: this.state.currentProduct.src,
-      category: this.state.currentProduct.category,
+    axios
+        .get(`${process.env.REACT_APP_API_URL}/products/user/${currentUser.id}/${id}`,config)
+        .then((response) => {
+          this.setState({ currentProduct: response.data });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  }
+
+  // Veröffentlichen des Produkts mit Axios-Fetch und JWT-Token
+  updatePublished(status) {
+    const { currentProduct } = this.state;
+    const currentUser = Auth.getCurrentUser();
+
+    if (!currentProduct) {
+      console.log("Kein Produkt ausgewählt");
+      return;
+    }
+
+    const updatedProduct = {
+      ...currentProduct,
       published: status,
     };
-    axios
-        .put(process.env.REACT_APP_API_URL +"/products/admin/" + this.state.currentProduct.id, data, config)
-        .then((response) => {
-          this.setState((prevState) => ({
-            currentProduct: {
-              ...prevState.currentProduct,
-              published: status,
-            },
-          }));
-          console.log(response.data);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-  }
 
-  // UPDATE fetch product with jwt token
-  updateProduct() {
     const config = {
-      headers:
-          authHeader()
+      headers: {
+        'Content-Type': 'application/json',
+        ...authHeader(),
+      },
     };
-    axios
-        .put(process.env.REACT_APP_API_URL +"/products/" + this.state.currentProduct.id, this.state.currentProduct, config)
-        .then((response) => {
-          console.log(response.data);
-          this.setState({
-            message: "The product was updated successfully!",
+    if (currentUser.roles.includes("ROLE_ADMIN")) {
+      axios
+          .put(
+              `${process.env.REACT_APP_API_URL}/products/admin/${currentProduct.id}`,
+              updatedProduct,
+              config
+          )
+          .then((response) => {
+            console.log(response.data);
+            this.setState((prevState) => ({
+              currentProduct: {
+                ...prevState.currentProduct,
+                published: status,
+              },
+            }));
+          })
+          .catch((error) => {
+            console.log("Fehler beim Aktualisieren des Produkts:", error);
           });
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+    } else {
+      axios
+          .put(
+              `${process.env.REACT_APP_API_URL}/products/user/${currentUser.id}/${currentProduct.id}`,
+              updatedProduct,
+              config
+          )
+          .then((response) => {
+            console.log(response.data);
+            this.setState((prevState) => ({
+              currentProduct: {
+                ...prevState.currentProduct,
+                published: status,
+              },
+            }));
+          })
+          .catch((error) => {
+            console.log("Fehler beim Aktualisieren des Produkts:", error);
+          });
+    }
   }
 
-  // delete fetch with jwt token
-  deleteProduct() {
-    let id = this.state.currentProduct.id;
+  // Produkt aktualisieren mit Axios-Fetch und JWT-Token
+  updateProduct() {
+    const { currentProduct } = this.state;
+    const currentUser = Auth.getCurrentUser();
+    if (!currentProduct) {
+      console.log("Kein Produkt ausgewählt");
+      return;
+    }
+
     const config = {
-      headers:
-          authHeader()
+      headers: authHeader(),
     };
-    axios
-        .delete(process.env.REACT_APP_API_URL +"/products/admin/" + id, config)
-        .then((response) => {
-          console.log(response.data);
-          this.props.router.push("/admin");
-          window.location.reload();
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+    if (currentUser.roles.includes("ROLE_ADMIN")) {
+      axios
+          .put(
+              `${process.env.REACT_APP_API_URL}/products/admin/${currentProduct.id}`,
+              currentProduct,
+              config
+          )
+          .then((response) => {
+            console.log(response.data);
+            this.setState({
+              message: "Das Produkt wurde erfolgreich aktualisiert!",
+            });
+          })
+          .catch((error) => {
+            console.log("Fehler beim Aktualisieren des Produkts:", error);
+          });
+    } else {
+      axios
+          .put(
+              `${process.env.REACT_APP_API_URL}/products/user/${currentUser.id}/${currentProduct.id}`,
+              currentProduct,
+              config
+          )
+          .then((response) => {
+            console.log(response.data);
+            this.setState({
+              message: "Das Produkt wurde erfolgreich aktualisiert!",
+            });
+          })
+          .catch((error) => {
+            console.log("Fehler beim Aktualisieren des Produkts:", error);
+          });
+    }
   }
 
+  // Produkt löschen mit Axios-Fetch und JWT-Token
+  deleteProduct() {
+    const { currentProduct } = this.state;
+    const currentUser = Auth.getCurrentUser();
+    const config = {
+      headers: authHeader(),
+    };
+    if (currentUser.roles.includes("ROLE_ADMIN")) {
+      axios
+          .delete(`${process.env.REACT_APP_API_URL}/products/admin/${currentUser.id}/${currentProduct.id}`,config)
+          .then((response) => response.data)
+          .then((data) => {
+            this.setState({ products: data });
+            this.props.router.navigate("/admin"); // Zurück zur Adminseite navigieren
+            window.location.reload(); // Seite neu laden
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+    } else {
+      axios
+          .delete(`${process.env.REACT_APP_API_URL}/products/user/${currentUser.id}/${currentProduct.id}`,config)
+          .then((response) => response.data)
+          .then((data) => {
+            this.setState({ products: data });
+            this.props.router.navigate("/products"); // Zurück zur Adminseite navigieren
+            window.location.reload(); // Seite neu laden
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+    }
+  }
 
   render() {
     const { currentProduct } = this.state;
     if (
-      this.state.currentProduct == null ||
-      this.state.currentProduct.length === 0
+        this.state.currentProduct == null ||
+        this.state.currentProduct.length === 0
     ) {
       return (
-        <h2 style={{ margin: "100px", textAlign: "center" }}>
-          this Product not exist
-        </h2>
+          <h2 style={{ margin: "100px", textAlign: "center" }}>
+            this Product not exist
+          </h2>
       );
     } else {
       return (
-        <div>
-          <h4>Product edit</h4>
+          <div>
+            <h4>Product edit</h4>
 
-          <div className="create_product">
-            <div className="upload">
-              <input
-                type="file"
-                name="file"
-                id="file_up"
-                defaultValue={currentProduct.src}
-                onChange={this.changeSrcHandler}
-              />
-            </div>
-
-            <form>
-              <div className="row">
-                <label htmlFor="title">Title</label>
+            <div className="create_product">
+              <div className="upload">
                 <input
-                  type="text"
-                  name="title"
-                  placeholder="Title"
-                  defaultValue={currentProduct.title}
-                  onChange={this.changeTitleHandler}
+                    type="file"
+                    name="file"
+                    id="file_up"
+                    defaultValue={currentProduct.src}
+                    onChange={this.changeSrcHandler}
                 />
               </div>
 
-              <div className="row">
-                <label htmlFor="price">Price</label>
-                <input
-                  type="number"
-                  name="price"
-                  placeholder="Price"
-                  defaultValue={currentProduct.price}
-                  onChange={this.changePriceHandler}
-                />
-              </div>
+              <form>
+                <div className="row">
+                  <label htmlFor="title">Title</label>
+                  <input
+                      type="text"
+                      name="title"
+                      placeholder="Title"
+                      defaultValue={currentProduct.title}
+                      onChange={this.changeTitleHandler}
+                  />
+                </div>
 
-              <div className="row">
-                <label htmlFor="stock">Stock</label>
-                <input
-                  type="number"
-                  name="stock"
-                  placeholder="how much in stock"
-                  defaultValue={currentProduct.stock}
-                  onChange={this.changeStockHandler}
-                />
-              </div>
+                <div className="row">
+                  <label htmlFor="price">Price</label>
+                  <input
+                      type="number"
+                      name="price"
+                      placeholder="Price"
+                      defaultValue={currentProduct.price}
+                      onChange={this.changePriceHandler}
+                  />
+                </div>
 
-              <div className="row">
-                <label htmlFor="description">Description</label>
-                <textarea
-                  type="text"
-                  name="description"
-                  id="description"
-                  placeholder="Description..."
-                  defaultValue={currentProduct.description}
-                  rows="5"
-                  onChange={this.changeDescriptionHandler}
-                />
-              </div>
+                <div className="row">
+                  <label htmlFor="stock">Stock</label>
+                  <input
+                      type="number"
+                      name="stock"
+                      placeholder="how much in stock"
+                      defaultValue={currentProduct.stock}
+                      onChange={this.changeStockHandler}
+                  />
+                </div>
 
-              <div className="row">
-                <label htmlFor="content">Content</label>
-                <textarea
-                  type="text"
-                  name="content"
-                  id="content"
-                  placeholder="Content..."
-                  defaultValue={currentProduct.content}
-                  rows="7"
-                  onChange={this.changeContentHandler}
-                />
-              </div>
+                <div className="row">
+                  <label htmlFor="description">Description</label>
+                  <textarea
+                      type="text"
+                      name="description"
+                      id="description"
+                      placeholder="Description..."
+                      defaultValue={currentProduct.description}
+                      rows="5"
+                      onChange={this.changeDescriptionHandler}
+                  />
+                </div>
 
-              <div className="row">
-                <label htmlFor="categories">Categories: </label>
-                <select
-                  name="category"
-                  value={currentProduct.category.id}
-                  onChange={this.changeCategoryHandler}
-                >
-                  <option defaultValue="">
-                    {currentProduct.category.name}
-                  </option>
-                  {this.state.categories.map((category) => (
-                    <option value={category.id} key={category.id}>
-                      {category.name}
+                <div className="row">
+                  <label htmlFor="content">Content</label>
+                  <textarea
+                      type="text"
+                      name="content"
+                      id="content"
+                      placeholder="Content..."
+                      defaultValue={currentProduct.content}
+                      rows="7"
+                      onChange={this.changeContentHandler}
+                  />
+                </div>
+
+                <div className="row">
+                  <label htmlFor="categories">Categories: </label>
+                  <select
+                      name="category"
+                      value={currentProduct.category.id}
+                      onChange={this.changeCategoryHandler}
+                  >
+                    <option defaultValue="">
+                      {currentProduct.category.name}
                     </option>
-                  ))}
-                </select>
-              </div>
-            </form>
-          </div>
-          <div className="button-group">
-            {currentProduct.published ? (
-              <Button onClick={() => this.updatePublished(false)}>
-                UnPublish
-              </Button>
-            ) : (
+                    {this.state.categories.map((category) => (
+                        <option value={category.id} key={category.id}>
+                          {category.name}
+                        </option>
+                    ))}
+                  </select>
+                </div>
+              </form>
+            </div>
+            <div className="button-group">
+              {currentProduct.published ? (
+                  <Button onClick={() => this.updatePublished(false)}>
+                    UnPublish
+                  </Button>
+              ) : (
+                  <Button
+                      buttonStyle="btn--normal"
+                      onClick={() => this.updatePublished(true)}
+                  >
+                    Publish
+                  </Button>
+              )}
+
               <Button
-                buttonStyle="btn--normal"
-                onClick={() => this.updatePublished(true)}
+                  buttonStyle="btn--delete"
+                  buttonSize={"btn--small"}
+                  onClick={this.deleteProduct}
               >
-                Publish
+                Delete
               </Button>
-            )}
 
-            <Button
-              buttonStyle="btn--delete"
-              buttonSize={"btn--small"}
-              onClick={this.deleteProduct}
-            >
-              Delete
-            </Button>
-
-            <Button
-              type="submit"
-              buttonStyle="btn--update"
-              buttonSize={"btn--small"}
-              onClick={this.updateProduct}
-            >
-              Update
-            </Button>
+              <Button
+                  type="submit"
+                  buttonStyle="btn--update"
+                  buttonSize={"btn--small"}
+                  onClick={this.updateProduct}
+              >
+                Update
+              </Button>
+            </div>
+            <p>{this.state.message}</p>
           </div>
-          <p>{this.state.message}</p>
-        </div>
       );
     }
   }
