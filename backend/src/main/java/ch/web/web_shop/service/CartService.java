1c0ee058a9f33f962a4671ca784af52ada7d2109
Sy Viet <sy.dao@cec.valantic.com>
2023-08-08 14:37:54 +0200

    CartService implemented

---
+++ backend/src/main/java/ch/web/web_shop/service/CartService.java
@@ -0,0 +1,20 @@
+package ch.web.web_shop.service;
+
+import ch.web.web_shop.exception.NotEnoughProductsInStockException;
+import ch.web.web_shop.model.Product;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public interface CartService {
+
+    void addProduct(Product product);
+
+    void removeProduct(Product product);
+
+    Map<Product, Integer> getProductsInCart();
+
+    void checkout() throws NotEnoughProductsInStockException;
+
+    BigDecimal getTotal();
+}
---
+++ backend/src/main/java/ch/web/web_shop/service/CartServiceImpl.java
@@ -0,0 +1,84 @@
+package ch.web.web_shop.service;
+
+import ch.web.web_shop.exception.NotEnoughProductsInStockException;
+import ch.web.web_shop.model.Product;
+import ch.web.web_shop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Service
+@Transactional
+public class CartServiceImpl implements CartService {
+
+    private ProductRepository productRepository;
+
+    private Map<Product, Integer> products = new HashMap<>();
+
+    @Autowired
+    public CartServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        if (products.containsKey(product)) {
+            products.replace(product, products.get(product) + 1);
+        } else {
+            products.put(product, 1);
+        }
+    }
+
+
+    @Override
+    public void removeProduct(Product product) {
+        if (products.containsKey(product)) {
+            if (products.get(product) > 1)
+                products.replace(product, products.get(product) - 1);
+            else if (products.get(product) == 1) {
+                products.remove(product);
+            }
+        }
+    }
+
+    @Override
+    public Map<Product, Integer> getProductsInCart() {
+        return Collections.unmodifiableMap(products);
+    }
+
+
+    @Override
+    public void checkout() throws NotEnoughProductsInStockException {
+        Product product;
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            // Refresh quantity for every product before checking
+            product = productRepository.findOne(entry.getKey().getId());
+            if (product.getStock() < entry.getValue())
+                throw new NotEnoughProductsInStockException(product);
+            entry.getKey().setStock(product.getStock() - entry.getValue());
+        }
+
+    }
+
+    @Override
+    public BigDecimal getTotal() {
+        BigDecimal total = BigDecimal.ZERO;
+
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            int quantity = entry.getValue();
+            BigDecimal price = BigDecimal.valueOf(product.getPrice());
+            BigDecimal subtotal = price.multiply(BigDecimal.valueOf(quantity));
+            total = total.add(subtotal);
+        }
+
+        return total;
+    }
+
+}
