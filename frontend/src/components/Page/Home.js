452cd924f94594b87320c40d8ed464b35f0702eb
Sy Viet <sy.dao@cec.valantic.com>
2023-08-31 16:31:27 +0200

    category function implement

--- frontend/src/components/Page/Home.js
+++ frontend/src/components/Page/Home.js
@@ -1,127 +1,177 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../Button";
 import "../css/Products.css";
 import axios from "axios";
 import authHeader from "../services/Auth-header";
 
-class Home extends React.Component {
+class Home extends Component {
   constructor(props) {
     super(props);
-    this.onChangeSearchTitle = this.onChangeSearchTitle.bind(this);
-    this.searchTitle = this.searchTitle.bind(this);
-
     this.state = {
       products: [],
-      currentProduct: null,
-      currentIndex: -1,
       searchTitle: "",
+      categoryTree: [],
+      selectedCategory: 'All', // Track the selected category
     };
+
+    this.onChangeSearchTitle = this.onChangeSearchTitle.bind(this);
+    this.searchTitle = this.searchTitle.bind(this);
+    this.searchCategory = this.searchCategory.bind(this);
+    this.addProductToCart = this.addProductToCart.bind(this);
+    this.handleCategoryChange = this.handleCategoryChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchProducts();
+    this.fetchCategories();
+  }
+
+  fetchProducts() {
+    fetch(process.env.REACT_APP_API_URL + "/products/published")
+        .then((response) => response.json())
+        .then((data) => this.setState({ products: data }));
+  }
+
+  fetchCategories() {
+    fetch(process.env.REACT_APP_API_URL + "/category/categoryTree")
+        .then((response) => response.json())
+        .then((data) => this.setState({ categoryTree: data }));
   }
 
   onChangeSearchTitle(e) {
-    const searchTitle = e.target.value;
-
     this.setState({
-      searchTitle: searchTitle,
+      searchTitle: e.target.value,
     });
   }
 
-  //sechrch for title
-  searchTitle() {
-    this.setState({
-      currentProduct: null,
-      currentIndex: -1,
-    });
-    let title = this.state.searchTitle;
-    fetch(process.env.REACT_APP_API_URL +"/products/published?title=" + title)
-      .then((response) => response.json())
-      .then((data) => this.setState({ products: data }));
-  }
-
-  // get all published products
-  componentDidMount() {
-    if (this.state.products == null || this.state.products.length === 0) {
-      fetch(process.env.REACT_APP_API_URL +"/products/published")
+  searchCategory(categoryId) {
+    fetch(process.env.REACT_APP_API_URL + `/category/products/${categoryId}`)
         .then((response) => response.json())
         .then((data) => this.setState({ products: data }));
-    }
+  }
+
+  searchTitle() {
+    const title = this.state.searchTitle;
+    fetch(process.env.REACT_APP_API_URL + "/products/published?title=" + title)
+        .then((response) => response.json())
+        .then((data) => this.setState({ products: data }));
   }
 
   addProductToCart(productId) {
     const config = {
-      headers: authHeader()
+      headers: authHeader(),
     };
 
     const payload = {
       productId: productId,
-      quantity: 1 // Anzahl kann nach Bedarf geändert werden
+      quantity: 1,
     };
 
-    axios.post(process.env.REACT_APP_API_URL + "/cart/add", payload, config)
+    axios
+        .post(process.env.REACT_APP_API_URL + "/cart/add", payload, config)
         .then(() => {
-          this.fetchCartData(); // Daten aktualisieren
-         window.location.reload();
+          this.fetchCartData(); // Update cart data
+          window.location.reload();
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
   }
 
+  handleCategoryChange(event) {
+    const selectedCategoryId = event.target.value;
+
+    // Call your fetchProductsByCategoryId function here
+    if (selectedCategoryId === 'All') {
+      // Fetch all products
+      this.fetchProducts();
+    } else {
+      // Fetch products by category ID
+      this.searchCategory(selectedCategoryId)
+    }
+
+    this.setState({ selectedCategory: selectedCategoryId });
+  }
 
   render() {
-    const { searchTitle, products } = this.state;
-    if (this.state.products == null || this.state.products.length === 0) {
-      return (
-        <h2 style={{ margin: "100px", textAlign: "center" }}>
-          No more products, all sold out
-        </h2>
-      );
-    } else {
-      return (
+    const { searchTitle, products, categoryTree, selectedCategory } = this.state;
+
+    return (
         <div>
           <h4>Welcome to shop.ch</h4>
           <div className="search">
             <div>
               <input
-                type="text"
-                className="search-input"
-                placeholder="Search by title"
-                value={searchTitle}
-                onChange={this.onChangeSearchTitle}
+                  type="text"
+                  className="search-input"
+                  placeholder="Search by title"
+                  value={searchTitle}
+                  onChange={this.onChangeSearchTitle}
               />
-
               <Button
-                buttonStyle="btn--normal"
-                buttonSize="btn-small"
-                type="button"
-                onClick={this.searchTitle}
+                  buttonStyle="btn--normal"
+                  buttonSize="btn-small"
+                  type="button"
+                  onClick={this.searchTitle}
               >
                 Search
               </Button>
+              <div className="search">
+                <div>
+                  <select
+                      className="form-control"
+                      id="category"
+                      name="category"
+                      value={selectedCategory}
+                      onChange={this.handleCategoryChange}
+                  >
+                    <option value="All">All</option>
+                    {categoryTree.map(category => (
+                        <React.Fragment key={category.id}>
+                          <option className="optionGroup" value={category.id}>
+                            {category.name}
+                          </option>
+                          {category.subCategory.map(subCategory => (
+                              <option
+                                  key={subCategory.id}
+                                  className="optionChild"
+                                  value={subCategory.id}
+                              >
+                                {subCategory.name}
+                              </option>
+                          ))}
+                        </React.Fragment>
+                    ))}
+                  </select>
+                </div>
+              </div>
+
             </div>
           </div>
 
           <div id="product">
             {products.map((product) => (
-              <div className="card" key={product.id}>
-                <Link to={`/products/detail/${product.id}`}>
-                  <img src=".\img\blob.jpg" alt="Product_Picture" />
-                </Link>
-                <div className="content">
+                <div className="card" key={product.id}>
                   <Link to={`/products/detail/${product.id}`}>
-                    <h3>{product.title}</h3>
+                    <img src={product.imagePaths[0]} alt="picture" />
                   </Link>
-                  <span>CHF {product.price}.00</span>
-                  <p>{product.description}</p>
-                  <button onClick={() => this.addProductToCart(product.id)}>Add to cart</button>
+                  <div className="content">
+                    <Link to={`/products/detail/${product.id}`}>
+                      <h3>{product.title}</h3>
+                    </Link>
+                    <span>CHF {product.price}.00</span>
+                    <p>{product.description}</p>
+                    <button onClick={() => this.addProductToCart(product.id)}>
+                      Add to cart
+                    </button>
+                  </div>
                 </div>
-              </div>
             ))}
           </div>
         </div>
-      );
-    }
+    );
   }
 }
+
 export default Home;
